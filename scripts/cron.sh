c7f55a3e3eb4001dfef9a06507883d834951b6f6
Helena Rasche <hxr@hx42.org>
2021-12-02 10:58:45 +0100

    no longer required

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
-
 # Dependencies
 pip install -r requirements.txt
-
-# Update the trusted tools.
-make fix
-make lint
-make update_trusted
-
-# Commit changes
-git config --global user.email "jenkins@galaxy.bioinformatics-atgm.nl"
-git config --global user.name "Jenkins Bot"
-git checkout main
-git add *.lock
-git commit -m "Updated trusted tools ($(date -I))" || true
-git push
 
 # Apply the changes
 make install GALAXY_SERVER=http://galaxy.bioinformatics-atgm.nl/ GALAXY_API_KEY=$GALAXY_ADMIN_API_KEY
