5fb6204dcfd52f03781dcc8cda5934a4971c5983
Helena Rasche <hxr@hx42.org>
2021-12-02 16:55:42 +0100

    disable old server

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -3,6 +3,5 @@
 pip install -r requirements.txt
 
 # Apply the changes
-make install GALAXY_SERVER=https://galaxy.bioinformatics-atgm.nl/ GALAXY_API_KEY=$GALAXY_ADMIN_API_KEY
 make install GALAXY_SERVER=https://galaxy.atgm.avans.nl/          GALAXY_API_KEY=$GALAXY_ADMIN_PROD_API_KEY
 make install GALAXY_SERVER=https://test-galaxy.atgm.avans.nl/     GALAXY_API_KEY=$GALAXY_ADMIN_DEV_API_KEY
