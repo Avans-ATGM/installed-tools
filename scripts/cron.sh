06545050e19eaf475068843fba558b69e30acea0
Donny Vrins <62692537+Dirowa@users.noreply.github.com>
2021-12-09 13:40:16 +0100

    Update cron.sh
    reverting back

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -4,4 +4,4 @@ pip install -r requirements.txt
 
 # Apply the changes
 make install GALAXY_SERVER=https://galaxy.atgm.avans.nl/          GALAXY_API_KEY=$GALAXY_ADMIN_PROD_API_KEY
-#make install GALAXY_SERVER=https://test-galaxy.atgm.avans.nl/     GALAXY_API_KEY=$GALAXY_ADMIN_DEV_API_KEY
+make install GALAXY_SERVER=https://test-galaxy.atgm.avans.nl/     GALAXY_API_KEY=$GALAXY_ADMIN_DEV_API_KEY
